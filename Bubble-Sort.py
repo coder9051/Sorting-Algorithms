0af8b4d251384ba87686f31fd51acf8825a3de1e
coder9051 <84278583+coder9051@users.noreply.github.com>
2022-03-14 19:37:21 +0530

    Update Bubble-Sort.py

--- Bubble-Sort.py
+++ Bubble-Sort.py
@@ -1,1 +1,23 @@
+def bubble_sort(array):
+    # Find length of array
+    n = len(array)
 
+    # Outer loop
+    for i in range(n-1):
+
+        # Inner loop
+        for j in range(n-1):
+
+            # check element if element is greater than next, it gets swapped
+            if array[j] > array[j+1]:
+
+                #Swapping
+                array[j], array[j+1] = array[j+1], array[j]
+
+                
+array = [8, 9, 6, 54, 2, 81, 8]
+
+# Call Function
+bubble_sort(array)
+
+print(array)
